f00fb8fa6ee7e5b3a9fcc326efa0fe941950fcf5
Maou Yami <maou@yamitec.com>
2021-02-25 02:21:46 -0300

    fix bug on Derivers Module n config Clients module

---
+++ api/database/migrations/2021_02_11_194249_create_files_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100)->nullable();
+            $table->string('name', 36);
+            $table->string('path', 200);
+
+            //$table->unsignedBigInteger('client_id')->nullable();
+            //$table->unsignedBigInteger('driver_id')->nullable();
+
+            //$table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+           // $table->foreign('driver_id')->references('id')->on('drivers')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
